402a87f0c571beb977675b71388185bad897b398
João Arthur <81393611+jarthurdev@users.noreply.github.com>
2024-01-05 16:43:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const scrollReveal = ScrollReveal ({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: false
+})
+
+ScrollReveal().reveal('.head.container', {delay: 500}, {interval: 100});
+
+ScrollReveal().reveal('.free.container', {delay: 1000}, {interval: 100});
+
+ScrollReveal().reveal('.how-it-works.container', {delay: 500}, {interval: 100});
+
+ScrollReveal().reveal('.about.container', {delay: 500}, {interval: 100});
+
+ScrollReveal().reveal('.infopainels.container', {delay: 500}, {interval: 100});
+
+ScrollReveal().reveal('footer.container', {delay: 500}, {interval: 100});
+
+activateMenuAtCurrentSection()
+
+
+
